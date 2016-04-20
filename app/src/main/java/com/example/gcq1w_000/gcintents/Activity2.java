71e30aa34cfd248d8ba1bb98103b4c15d805e3fe
Glenn Chon <glenn.chon@gmail.com>
2016-04-20 15:12:13 -0400

    commit 1

---
+++ app/src/main/java/com/example/gcq1w_000/gcintents/Activity2.java
@@ -0,0 +1,43 @@
+package com.example.gcq1w_000.gcintents;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * Created by Gcq1w_000 on 3/14/2016.
+ */
+public class Activity2 extends MainActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity1);
+
+        //---get the OK button---
+        Button btn = (Button) findViewById(R.id.submitBtn);
+
+        //---event handler for the OK button---
+        btn.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view) {
+                Intent data = new Intent();
+
+                //---get the EditText view---
+                EditText txt_username =
+                        (EditText) findViewById(R.id.nameEditText);
+
+                //---set the data to pass back---
+                data.setData(Uri.parse(
+                        txt_username.getText().toString()));
+                setResult(RESULT_OK, data);
+
+                //---closes the activity---
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/gcq1w_000/gcintents/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.gcq1w_000.gcintents;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    int request_Code = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onAct1BtnClick(View view){
+        Intent Activity1 = new Intent(this, Activity1.class);
+        startActivityForResult(Activity1, request_Code);
+    }
+    public void onAct2BtnClick(View view){
+
+        startActivityForResult(new Intent(
+                        "android.intent.action.ACTIVITY1"),
+                request_Code);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == request_Code) {
+            if(resultCode == RESULT_OK) {
+                Toast.makeText(this, data.getData().toString(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
